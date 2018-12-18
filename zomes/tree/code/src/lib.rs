de994f3eba02d60f01cb6e330c669f135e90ebd7
Charles Cunningham <charles.cunningham@elexonelectronics.com>
2018-12-18 22:46:00 +0100

    added get_replies but probably doesnt work

--- zomes/tree/code/src/lib.rs
+++ zomes/tree/code/src/lib.rs
@@ -108,3 +108,28 @@ fn handle_create_reply(parent_addr: Address, reply: Comment) -> JsonString {
             Err(hdk_err) => hdk_err.into()
         }
 }
+
+fn handle_get_replies(parent_addr: Address) -> JsonString {
+    // try and get the parent entry and ensure it is the data type we expect
+    let maybe_parent = hdk::get_entry(parent_addr.clone())
+        .map(|entry| Comment::try_from(entry.unwrap().value()));
+
+	match maybe_parent {
+		Ok(Ok(parent)) => {
+
+            // try and load the replies and convert them to the correct struct
+            // please forgive the unwraps
+			let replies = hdk::get_links(&parent_addr, "replies").unwrap().addresses()
+                .iter()
+                .map(|reply_address| {
+                    let entry = hdk::get_entry(reply_address.to_owned()).unwrap().unwrap();
+                    Comment::try_from(entry.value().clone()).unwrap()
+                }).collect::<Vec<Comment>>();
+
+            // if this was successful for all list items then return them
+            json!({"name": parent.name, "replies": list_items}).into()
+
+		},
+        _ => json!({"successs": false, "message": "No comment at this address"}).into()
+	}
+}
