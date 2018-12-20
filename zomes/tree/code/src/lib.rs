17bcb21747013f1bdf8569c16d5ba3b1d9407180
Charles Cunningham <charles.cunningham@outlook.com>
2018-12-20 21:28:12 +0100

    heapsa changes, added root entry of comment type, registered functions, other stiff

--- zomes/tree/code/src/lib.rs
+++ zomes/tree/code/src/lib.rs
@@ -3,57 +3,64 @@ use std::convert::TryFrom;
 
 #[macro_use]
 extern crate hdk;
+extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
-extern crate holochain_core_types_derive;
-#[macro_use]
 extern crate serde_json;
-use hdk::holochain_core_types::{
-    hash::HashString,
-    error::HolochainError,
-    entry::Entry,
-    dna::zome::entry_types::Sharing,
-    entry::entry_type::EntryType,
-    json::JsonString,
-    cas::content::Address
+#[macro_use]
+extern crate holochain_core_types_derive;
+
+
+// use hdk::holochain_core_types::{
+//     // hash::HashString,
+//     error::HolochainError,
+//     entry::Entry,
+//     dna::zome::entry_types::Sharing,
+//     entry::entry_type::EntryType,
+//     json::JsonString,
+//     cas::content::Address
+//     // json::DefaultJson
+// };
+
+use hdk::{
+    error::ZomeApiResult,
+    holochain_core_types::{cas::content::Address, entry::Entry, json::JsonString, error::HolochainError},
+    holochain_wasm_utils::api_serialization::get_links::GetLinksResult,
 };
-
-enum VoteValue { up, down }
-
 // for entries this simple, and for vote organising, we need to record some metadata to prevent collisions (especially with something as simple as votes)
 // a timestamp or order record should be enough, as long as it's something outside the control of the user (to prevent intentional collisions for whatever reason)
 
-#[derive(Serialize, Deserialize, Debug, DefaultJson)]
 // each comment is content and a commiting actor
 // TODO figure out if the commiting actor can be discovered without storing it in the entry
+#[derive(Serialize, Deserialize, Debug, DefaultJson)]
 struct Comment {
     content: String,
     committer: String,
     timestamp: u64,
 }
 
-#[derive(Serialize, Deserialize, Debug, DefaultJson)]
-// each vote is just a simple enum value, changing a vote probably requires an update or delete/commit cycle
+// each vote is just a simple bool (true == upvote, false == downvote), changing a vote probably requires an update or delete/commit cycle
 // afaik, updating an entry leave it and appends a pointer to the updated entry which the DHT retrieves
 // deleting I think appends a notice telling everyone's local HT to just ignore it (or even to drop it from the table?)
 // imo, deleting is a better option for managing DHT size, even if votes are small, 10k of them could still be a few Mbs
 // TODO as above with the comment/actor thing
+#[derive(Serialize, Deserialize, Debug, DefaultJson)]
 struct Vote {
-    value: VoteValue,
+    value: bool,
     voter: String,
     timestamp: u64,
 }
 
 define_zome! {
 entries: [
-        entry!(
-            name: "comment",
-            description: "A comment in the tree",
+            entry!(
+            name: "root",
+            description: "An initial parentless topic",
             sharing: Sharing::Public,
             native_type: Comment,
             validation_package: || hdk::ValidationPackageDefinition::Entry,
-            validation: |list: List, _ctx: hdk::ValidationData| {
+            validation: |comment: Comment, _ctx: hdk::ValidationData| {
                 Ok(())
             },
             links: [
@@ -72,8 +79,36 @@ entries: [
                     validation: |base: Address, target: Address, _ctx: hdk::ValidationData| {
                         Ok(())
                     }
+                )
+            ]
+        ),
+        entry!(
+            name: "comment",
+            description: "A comment in the tree",
+            sharing: Sharing::Public,
+            native_type: Comment,
+            validation_package: || hdk::ValidationPackageDefinition::Entry,
+            validation: |comment: Comment, _ctx: hdk::ValidationData| {
+                Ok(())
+            },
+            links: [
+                to!(
+                    "Vote",
+                    tag: "votes",
+                    validation_package: || hdk::ValidationPackageDefinition::Entry,
+                    validation: |base: Address, target: Address, _ctx: hdk::ValidationData| {
+                        Ok(())
+                    }
                 ),
-                // is delcaring a from! one way identicle to declaring a to! the other way?
+                to!(
+                    "Reply",
+                    tag: "replies",
+                    validation_package: || hdk::ValidationPackageDefinition::Entry,
+                    validation: |base: Address, target: Address, _ctx: hdk::ValidationData| {
+                        Ok(())
+                    }
+                )
+                // is declaring a from! one way identicle to declaring a to! the other way?
                 // TODO figure that out/wait for more documentation
                 // from!(
                 //     "Parent",
@@ -93,7 +128,7 @@ entries: [
             // TODO validate so each person can only vote once, changing votes is possible though
             // we'd have to have that validation done during linking, not committing? or in the handling function?
             validation_package: || hdk::ValidationPackageDefinition::Entry,
-            validation: |list_item: ListItem, _ctx: hdk::ValidationData| {
+            validation: |vote: Vote, _ctx: hdk::ValidationData| {
                 Ok(())
             }
         )
@@ -101,20 +136,54 @@ entries: [
 
     genesis: || { Ok(()) }
 
-    functions: {}
+    functions: {
+        main (Public) {
+            create_root: {
+                inputs: |root: Comment|,
+                outputs: |result: JsonString|,
+                handler: handle_create_root
+            }
+            get_roots: {
+                inputs: | |,
+                outputs: |results: ZomeApiResult<Vec<Address>>|,
+                handler: handle_get_roots
+            }
+            create_reply: {
+                inputs: |parent_addr: Address, reply: Comment|,
+                outputs: |result: JsonString|,
+                handler: handle_create_reply
+            }
+            get_replies: {
+                inputs: |parrent_addr: Address|,
+                outputs: |result: JsonString|,
+                handler: handle_get_replies
+            }
+            apply_vote: {
+                inputs: |target_comment_addr: Address, vote: Vote|,
+                outputs: |results: JsonString|,
+                handler: handle_apply_vote
+            }
+        }
+    }
 }
 
 // roots will have no visible parents. is an invisible "true root" necessary for finding the visible roots?
 // something with a very simple entry that the address can be easily found?
+// nah a query can be run for the "root" entries :)
 fn handle_create_root(root: Comment) -> JsonString {
     // create root entry
-    let root_entry = Entry::new(EntryType::App("comment".into()), root);
+    let root_entry = Entry::new(EntryType::App("root".into()), root);
 
     // commit it
 	match hdk::commit_entry(&root_entry) {
 		Ok(address) => json!({"success": true, "address": address}).into(),
 		Err(hdk_err) => hdk_err.into()
 	}
+}
+
+fn handle_get_roots() -> ZomeApiResult<Vec<Address>> {
+    // get root addresses
+    return hdk::query("root", 0, 0)
 }
 
 fn handle_create_reply(parent_addr: Address, reply: Comment) -> JsonString {
@@ -141,7 +210,7 @@ fn handle_get_replies(parent_addr: Address) -> JsonString {
 
     // check if it is
 	match maybe_parent {
-		Ok(Ok(parent)) => {
+		Ok(Ok(_parent)) => {
 
             // try and load the replies and convert them to the correct struct
             // please forgive the unwraps
@@ -153,7 +222,7 @@ fn handle_get_replies(parent_addr: Address) -> JsonString {
                 }).collect::<Vec<Comment>>();
 
             // if this was successful for all list items then return them
-            json!({"name": parent.name, "replies": list_items}).into()
+            json!({"replies": replies}).into()
 
 		},  // return generic comment-not-found error if wrong type or doesnt exist or whatever
         _ => json!({"successs": false, "message": "No comment at this address"}).into()
