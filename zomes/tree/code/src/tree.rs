a4363ae00fdf67d81768f4162548d9e6f811dc73
Charles Cunningham <charles.cunningham@outlook.com>
2018-12-29 23:00:57 +0100

    tons of changes and it still dosent build and I cant remember them all

--- zomes/tree/code/src/tree.rs
+++ zomes/tree/code/src/tree.rs
@@ -50,7 +50,7 @@ pub fn root_definition() -> ValidatingEntryType {
 // roots will have no visible parents. is an invisible "true root" necessary for finding the visible roots?
 // something with a very simple entry that the address can be easily found?
 // nah a query can be run for the "root" entries :)
-pub fn handle_create_root(root: Comment) -> ZomeApiResult<Address> {
+pub fn create_root(root: Comment) -> ZomeApiResult<Address> {
     // create root entry
     let root_entry = Entry::new(EntryType::App("root".into()), root);
 
@@ -58,12 +58,12 @@ pub fn handle_create_root(root: Comment) -> ZomeApiResult<Address> {
 	return hdk::commit_entry(&root_entry)
 }
 
-pub fn handle_get_roots() -> ZomeApiResult<Vec<Address>> {
+pub fn get_roots() -> ZomeApiResult<Vec<Address>> {
     // get root addresses
-    return hdk::query("root", 0, 0)
+    return hdk::query("root".into(), 0, 0)
 }
 
-pub fn handle_create_reply(parent_addr: Address, reply: Comment) -> ZomeApiResult<Address> {
+pub fn create_reply(parent_addr: Address, reply: Comment) -> ZomeApiResult<Address> {
     // create reply entry
     let reply_entry = Entry::new(EntryType::App("comment".into()), reply);
 
@@ -74,45 +74,27 @@ pub fn handle_create_reply(parent_addr: Address, reply: Comment) -> ZomeApiResul
             })
 }
 
-pub fn handle_get_replies(parent_addr: Address) -> ZomeApiResult<Vec<Address>> {
-    // try and get the parent entry and ensure it is the data type we expect
-    let maybe_parent = hdk::get_entry(parent_addr.clone())
-        .map(|entry| Comment::try_from(entry.unwrap().value()));
-
-    // check if it is
-	match maybe_parent {
-		Ok(Ok(_parent)) => {
-
-            // try and load the replies and convert them to the correct struct
-            // please forgive the unwraps
-			let replies = hdk::get_links(&parent_addr, "replies").unwrap().addresses()
-                .iter()
-                .map(|reply_address| {
-                    let entry = hdk::get_entry(reply_address.to_owned()).unwrap().unwrap();
-                    Comment::try_from(entry.value().clone()).unwrap()
-                }).collect::<Vec<Comment>>();
-
-            // if this was successful for all list items then return them
-            json!({"replies": replies}).into()
-
-		},  // return generic comment-not-found error if wrong type or doesnt exist or whatever
-        _ => json!({"successs": false, "message": "No comment at this address"}).into()
-	}
+pub fn get_reply_addresses(parent_addr: Address) -> ZomeApiResult<GetLinksResult> {
+    return hdk::get_links(&parent_addr, "replies")
 }
 
-pub fn handle_apply_vote(target_comment_addr: Address, vote: Vote) -> ZomeApiResult<Address> {
+pub fn get_comment(comment_addr: Address) -> ZomeApiResult<Option<Entry>> {
+    return hdk::get_entry(&comment_addr)
+}
+
+pub fn apply_vote(target_comment_addr: Address, vote: Vote) -> ZomeApiResult<Address> {
     // create vote entry
     let vote_entry = Entry::new(EntryType::App("vote".into()), vote);
 
     // commit entry and link on success (should the check happen here?)
-    match hdk::commit_entry(&vote_entry)
+    return hdk::commit_entry(&vote_entry)
             .and_then(|vote_addr| {
                 hdk::link_entries(&target_comment_addr, &vote_addr, "replies")
             })
-        {
-            Ok(_) => {
-                json!({"success": true}).into()
-            },
-            Err(hdk_err) => hdk_err.into()
-        }
 }
+
+pub fn get_vote_addresses(comment: Address) -> ZomeApiResult<GetLinksResult> {
+    return hdk::get_links(&comment, "votes")
+}
+
+pub fn get_vote()
